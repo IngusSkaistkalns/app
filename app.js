95dd103d1a4512420c06342999596d6d8eabf805
jankosacc <janis.koselevs@accenture.com>
2016-11-22 22:30:52 +0200

    Updating main app files

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
+import React from 'react';
+import { Router, Route, Link, IndexRoute, hashHistory, browserHistory } from 'react-router';
+
+import LoginPage from './pages/LoginPage';
+import AboutPage from './pages/AboutPage';
+
 require('./styles/styles.less');
+
+class App extends React.Component {
+  render() {
+    return (
+      <Router history={hashHistory}>
+        <Route path='/' component={LoginPage} />
+        <Route path='/about' component={AboutPage} />
+      </Router>
+    )
+  }
+}
+
+export default App;
